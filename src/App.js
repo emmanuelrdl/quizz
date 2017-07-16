a54f992062c4d2a2bbd4f43686d0cee1ae85ff97
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2017-07-16 19:01:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+
+//http://rants.broonix.ca/getting-started-with-react-native-and-redux/
+
+export default class App extends React.Component {
+
+  render(){
+    return (
+      <View >
+        <Text >
+          coucou
+        </Text>
+      </View>
+    )
+  }
+}
