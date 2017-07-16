4204440b972349e48320d1e677a107ce07fdc569
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2017-07-16 19:28:14 +0200

    add redux

--- index.ios.js
+++ index.ios.js
@@ -11,8 +11,17 @@ import {
   Text,
   View
 } from 'react-native';
+import { Provider } from 'react-redux'
+import configureStore from './src/configureStore'
+import App from './src/app';
 
-import App from './src/App';
+const store = configureStore()
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 
-AppRegistry.registerComponent('quizz', () => App);
+AppRegistry.registerComponent('quizz', () => ReduxApp);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import {
-  Platform,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
-} from 'react-native';
+//https://medium.com/react-native-training/redux-4-ways-95a130da0cdc
 
+import React from 'react'
+import { TouchableHighlight, View, Text, StyleSheet } from 'react-native'
 
-//http://rants.broonix.ca/getting-started-with-react-native-and-redux/
+import { connect } from 'react-redux'
+import { fetchData } from './actions'
 
-export default class App extends React.Component {
+let styles
 
-  render(){
-    return (
-      <View >
-        <Text >
-          coucou
-        </Text>
-      </View>
-    )
+const App = (props) => {
+  const {
+    container,
+    text,
+    button,
+    buttonText
+  } = styles
+
+  return (
+    <View style={container}>
+      <Text style={text}>Redux Examples</Text>
+      <TouchableHighlight style={button}>
+        <Text style={buttonText}>Load Data</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+styles = StyleSheet.create({
+  container: {
+    marginTop: 100
+  },
+  text: {
+    textAlign: 'center'
+  },
+  button: {
+    height: 60,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0b7eff'
+  },
+  buttonText: {
+    color: 'white'
+  }
+})
+
+function mapStateToProps (state) {
+  return {
+    appData: state.appData
   }
 }
+
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchData: () => dispatch(fetchData())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
